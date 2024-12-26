f9d529feb64a407b5c11e043be3e832a5955a267
Regiane  Miranda Cabral de Jesus <111747423+giannycabral@users.noreply.github.com>
2024-12-26 20:47:25 -0300

    Update program02.cpp
    organização do código, adicionando para números inteiros.

--- program02.cpp
+++ program02.cpp
@@ -1,20 +1,20 @@
 #include<stdio.h>
 
 int main(){
-    float num1, num2;
+    int num1, num2;
     
-    printf("Digite o primeiro número real:");
-    scanf("%f", &num1);
+    printf("Digite o primeiro numero inteiro:");
+    scanf("%d", &num1);
     
-    printf("Digite o segundo número real:");
-    scanf("%f", &num2);
+    printf("Digite o segundo numero inteiro:");
+    scanf("%d", &num2);
     
-    printf("A soma é %.2f\n",num1+num2);
-    printf("A subtração é %.2f\n",num1-num2);
-    printf("A Multiplicação é %.2f\n",num1*num2);
+    printf("A soma é %d\n",num1+num2);
+    printf("A subtração é %d\n",num1-num2);
+    printf("A Multiplicação é %d\n",num1*num2);
     if(num2 != 0)
-        printf("A Divisão é %.2f\n",num1/num2);
+        printf("A Divisão é %d\n",num1/num2);
     else
-        printf("Não é possível dividir por zero.\n");
+        printf("Não é possivel dividir por zero.\n");
     return 0;
 }
